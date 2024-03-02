d9df642cc0c0b0f90ea1927aa01064a9a52f1cc2
subham das <das1998lipun@gmail.com>
2024-03-02 11:14:37 +0530

    made h/v scroll area

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Image,
+  View
+} from 'react-native';
 
-export default function App() {
+export default function YourComponentName() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView horizontal={true} style={styles.imageContainer}>
+        <View style={styles.imageSection}>
+          <Image
+            source={{
+              uri: 'https://media.istockphoto.com/id/1404885250/photo/mahabaleshwar-and-panchagani-nature-and-hill-areas.jpg?s=2048x2048&w=is&k=20&c=Su2wYszfDTThmt7AcCA5OHfGcqFVBxy73W-5YtemT28=',
+            }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+          <Image
+            source={{
+              uri: 'https://media.istockphoto.com/id/1404885250/photo/mahabaleshwar-and-panchagani-nature-and-hill-areas.jpg?s=2048x2048&w=is&k=20&c=Su2wYszfDTThmt7AcCA5OHfGcqFVBxy73W-5YtemT28=',
+            }}
+            style={styles.image}
+            resizeMode="cover"
+          />
+        </View>
+      </ScrollView>
+      <SafeAreaView style={styles.textSection}>
+        <ScrollView style={styles.scrollView}>
+          <Text style={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'black',
+  },
+  scrollView: {
+    backgroundColor: '#000000',
+    marginHorizontal: 20,
+  },
+  text: {
+    fontSize: 42,
+    color: 'white',
+    padding: 20,
+  },
+  imageContainer: {
+    backgroundColor: 'black',
+    maxHeight: '35%'
+  },
+  imageSection: {
+    flexDirection: 'row',
+
+  },
+  image: {
+    width: 400, // Adjust as needed
+    height: 300,
+    marginHorizontal: 5, // Adjust spacing between images
+  },
+  textSection: {
+    flex: 1,
   },
 });
